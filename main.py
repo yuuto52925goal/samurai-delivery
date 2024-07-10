689abfbd3641045aa19a3e44eebf87e8cba04ce9
yuuto52925goal <yuuto52925goal@gmail.com>
2024-07-10 09:37:03 +0900

    adding a key

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import random
 from flask_cors import CORS
-from flask import Flask, abort, render_template, redirect, url_for, flash, jsonify, send_file
+from flask import Flask, abort, render_template, redirect, url_for, flash, jsonify, send_file, request
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from flask_gravatar import Gravatar
@@ -9,13 +9,32 @@ from flask_login import UserMixin, login_user, LoginManager, current_user, logou
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Text
+from functools import wraps
+from dotenv import load_dotenv
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+load_dotenv()
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KYE')
+yuto = os.environ.get('USER_YUTO')
+app.config['API_KEYS'] = ['your_api_key1', 'your_api_key2', 'your_api_key3', yuto]
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 CORS(app)
+
+
+def require_api_key(view_function):
+    @wraps(view_function)
+    def decorated_function(*args, **kwargs):
+        if 'api_key' in request.args:
+            api_key = request.args.get('api_key')
+            if api_key in app.config['API_KEYS']:
+                return view_function(*args, **kwargs)
+            else:
+                return jsonify({"error": "Invalid API key"}), 403
+        else:
+            return jsonify({"error": "API key required"}), 401
+    return decorated_function
 
 
 @app.route("/")
@@ -24,6 +43,7 @@ def index():
 
 
 @app.route("/image/random")
+@require_api_key
 def random_image():
     try:
         files = os.listdir("./samurai_hero_image")
